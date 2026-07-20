69d5f5507b63f44eb5c62965c3ba8f3bb33778be
Alan Silva <alanweb7@gmail.com>
2026-07-20 18:44:56 -0300

    Home: renomear 'Mais Amados' para 'Queridinhos' + build assets

--- public/build/assets/AnalyticComponent-DfsUePWm.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,r as n,k as t,o as r}from"./app-BL5AM-Zd.js";const c={name:"AnalyticComponent"};function s(a,p,_,i,m,f){const o=n("router-view");return r(),t(o)}const u=e(c,[["render",s]]);export{u as default};
