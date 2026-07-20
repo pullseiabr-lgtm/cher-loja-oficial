2130622535e423cdea51dbf407ef59ff0f404ec6
Alan Silva <alanweb7@gmail.com>
2026-07-20 18:25:27 -0300

    Home: renomear seção 'Mais Popular' para 'Mais Amados' + build assets

---
+++ public/build/assets/ReturnReasonComponent-DtBekHX1.js
@@ -0,0 +1,1 @@
+import{_ as o,r as n,k as t,o as r}from"./app-BL5AM-Zd.js";const s={name:"ReturnReasonComponent"};function a(c,p,_,m,u,f){const e=n("router-view");return r(),t(e)}const d=o(s,[["render",a]]);export{d as default};
